70b49d6cb902adbe180b3808a33ce76174b5c9ad
MeryKitty <anhmdq99@gmail.com>
2020-07-11 15:09:13 +0700

    stuffs

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,27 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		
-		String pathToCsv = "C:\\Users\\Nam Le\\Shopee_Code_League\\Week5\\delivery_orders_march.csv";
-		String javaPathToCsv = pathToCsv.replace("\\", "/");
-		String pathToSampleCsv = "C:\\Users\\Nam Le\\Shopee_Code_League\\Week5\\delivery_orders_march_sample.csv";
-		String javaPathToSampleCsv = pathToSampleCsv.replace("\\", "/");
+		String pathToCsv = "./data/delivery_orders_march.csv";
+		String pathToSampleCsv = "./data/delivery_orders_march_sample.csv";
+		String outputFile = "./data/output.csv";
 		
-		BufferedReader csvReader = new BufferedReader(new FileReader(javaPathToCsv));
-		FileWriter sampleCsvWriter = new FileWriter(javaPathToSampleCsv);
-		String row;
+		BufferedReader csvReader = new BufferedReader(new FileReader(pathToCsv));
+		FileWriter sampleCsvWriter = new FileWriter(pathToSampleCsv);
+		String rowString;
 
-		String header = csvReader.readLine(); // throw this away
+		csvReader.readLine(); // throw the header away
 		
 		int count = 0;
-		while ((row = csvReader.readLine()) != null && count < 10) {
+		while ((rowString = csvReader.readLine()) != null && count < 10) {
 			count++;
-			sampleCsvWriter.append(row);
+			sampleCsvWriter.append(rowString);
 			sampleCsvWriter.append("\n");
 		    // String[] data = row.split(",");
 		    // do something with the data
@@ -33,20 +33,30 @@ public class Main {
 		
 		csvReader.close();
 
-		BufferedReader sampleCsvReader = new BufferedReader(new FileReader(javaPathToSampleCsv));
+		BufferedReader sampleCsvReader = new BufferedReader(new FileReader(pathToCsv));
+		sampleCsvReader.readLine();
+		
 		
 		List<Order> orders = new ArrayList<>();
 		
-		while ((row = sampleCsvReader.readLine()) != null) {
-		    String[] data = row.split(",");
-		    // do something with the data
-		    orders.add(new Order(data));
-		    
+		while ((rowString = sampleCsvReader.readLine()) != null) {
+		    orders.add(new Order(rowString));   
 		}
 		sampleCsvReader.close();
 		
-		for (Order o : orders) {
-			System.out.println(o.toString());
+		StringBuilder outputBuilder = new StringBuilder();
+		outputBuilder.append("orderid,is_late").append("\n");
+		for (Order order : orders) {
+			outputBuilder.append(order.orderId + ",");
+			if (order.check()) {
+				outputBuilder.append("0\n");
+			} else {
+				outputBuilder.append("1\n");
+			}
 		}
+		
+		PrintStream output = new PrintStream(outputFile);
+		output.append(outputBuilder.toString());
+		output.close();
 	}
 }
--- src/Order.java
+++ src/Order.java
@@ -1,19 +1,33 @@
-import java.sql.Timestamp;
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.List;
 
 public class Order {
-	Timestamp orderId;
-	Timestamp pick;
-	Timestamp firstDeliverAttempt;
-	Timestamp secondDeliverAttempt;
-	City buyerCity;
-	City sellerCity;
+	static List<LocalDate> HOLIDAYS = Arrays.asList(
+			LocalDate.of(2020, 3, 8),
+			LocalDate.of(2020, 3, 25),
+			LocalDate.of(2020, 3, 30),
+			LocalDate.of(2020, 3, 31)
+	);
 	
-	public Order(String[] row) {
-		orderId = new Timestamp(Long.parseLong(row[0]));
-		pick = new Timestamp(Long.parseLong(row[1]));
-		firstDeliverAttempt = new Timestamp((long)Double.parseDouble(row[2]));
-		if (!row[3].isEmpty()) {
-			secondDeliverAttempt = new Timestamp((long)Double.parseDouble(row[3]));
+	long orderId;
+	LocalDate pick, firstAttempt, secondAttempt;
+	City buyerCity, sellerCity;
+	
+	public Order(String rowString) {
+		String[] row = rowString.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+		if (row.length != 6) {
+			throw new AssertionError();
+		}
+		
+		orderId = Long.parseLong(row[0]);
+		pick = LocalDate.ofInstant(Instant.ofEpochSecond(Long.parseLong(row[1])), ZoneId.of("GMT+8"));
+		firstAttempt = LocalDate.ofInstant(Instant.ofEpochSecond((long)Double.parseDouble(row[2])), ZoneId.of("GMT+8"));
+		if (row[3] != null && !row[3].isEmpty()) {
+			secondAttempt = LocalDate.ofInstant(Instant.ofEpochSecond((long)Double.parseDouble(row[3])), ZoneId.of("GMT+8"));
 		}
 		
 		String buyerAddress = row[4].toLowerCase();
@@ -25,14 +39,19 @@ public class Order {
 		buyerAddress = buyerAddress.endsWith("\"") ? buyerAddress.substring(0, buyerAddress.length() - 1) : buyerAddress;
 		sellerAddress = sellerAddress.endsWith("\"") ? sellerAddress.substring(0, sellerAddress.length() - 1) : sellerAddress;
 		
+		buyerAddress = buyerAddress.strip();
+		sellerAddress = sellerAddress.strip();
+		
 		if (buyerAddress.endsWith("metro manila")) {
 			buyerCity = City.MetroManila;
 		} else if (buyerAddress.endsWith("luzon")) {
 			buyerCity = City.Luzon;
 		} else if (buyerAddress.endsWith("visayas")) {
 			buyerCity = City.Visayas;
-		} else {
+		} else if (buyerAddress.endsWith("mindanao")){
 			buyerCity = City.Mindanao;
+		} else {
+			throw new AssertionError(buyerAddress, null);
 		}
 		
 		if (sellerAddress.endsWith("metro manila")) {
@@ -41,36 +60,102 @@ public class Order {
 			sellerCity = City.Luzon;
 		} else if (sellerAddress.endsWith("visayas")) {
 			sellerCity = City.Visayas;
-		} else {
+		} else if (sellerAddress.endsWith("mindanao")) {
 			sellerCity = City.Mindanao;
+		} else {
+			throw new AssertionError(sellerAddress, null);
+		}
+		if (pick.getYear() != 2020 || firstAttempt.getYear() != 2020 || (secondAttempt != null && secondAttempt.getYear() != 2020)) {
+			throw new AssertionError();
+		}
+		if (!workDate(pick) || !workDate(firstAttempt) || (secondAttempt != null && !workDate(secondAttempt))) {
+			throw new AssertionError();
 		}
 	}
+	
+	static boolean workDate(LocalDate date) {
+		return !HOLIDAYS.contains(date) && !date.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+	}
+	
+	static int getDays(City sellerCity, City buyerCity) {
+		if (sellerCity == City.MetroManila && buyerCity == City.MetroManila) {
+			return 3;
+		} else if (sellerCity == City.MetroManila && buyerCity == City.Luzon ||
+				sellerCity == City.Luzon && buyerCity == City.MetroManila ||
+				sellerCity == City.Luzon && buyerCity == City.Luzon) {
+			return 5;
+		} else {
+			return 7;
+		}
+	}
+	
+	static LocalDate incrementDay(LocalDate day) {
+		LocalDate result = null;
+		LocalDate temp = day;
+		while (true) {
+			temp = temp.plusDays(1);
+			if (workDate(temp)) {
+				result = temp;
+				break;
+			}
+		}
+		return result;
+	}
+	
+	public boolean check() {
+		int SLA = getDays(sellerCity, buyerCity);
+		boolean firstAttemptOnTime = false;
+		LocalDate temp = this.pick;
+		for (int day = 1; day <= SLA; day++) {
+			temp = incrementDay(temp);
+			if (temp.equals(this.firstAttempt)) {
+				firstAttemptOnTime = true;
+				break;
+			}
+		}
+		if (!firstAttemptOnTime) {
+			return false;
+		}
+		if (this.secondAttempt == null) {
+			return true;
+		}
+		boolean secondAttemptOnTime = false;
+		temp = this.firstAttempt;
+		for (int day = 1; day <= 3; day++) {
+			temp = incrementDay(temp);
+			if (temp.equals(secondAttempt)) {
+				secondAttemptOnTime = true;
+				break;
+			}
+		}
+		return secondAttemptOnTime;
+	}
 
-	public Timestamp getOrderId() {
+	public long orderId() {
 		return orderId;
 	}
 
-	public Timestamp getPick() {
+	public LocalDate pick() {
 		return pick;
 	}
 
-	public Timestamp getFirstDeliverAttempt() {
-		return firstDeliverAttempt;
+	public LocalDate firstAttempt() {
+		return firstAttempt;
 	}
 
-	public Timestamp getSecondDeliverAttempt() {
-		return secondDeliverAttempt;
+	public LocalDate secondAttempt() {
+		return secondAttempt;
 	}
 
-	public City getBuyerCity() {
+	public City buyerCity() {
 		return buyerCity;
 	}
 
-	public City getSellerCity() {
+	public City sellerCity() {
 		return sellerCity;
 	}
 	
 	public String toString() {
-		return "" + orderId + pick + firstDeliverAttempt + secondDeliverAttempt + buyerCity + sellerCity;
+		return "" + orderId + "\t" + pick + "\t" + firstAttempt + "\t" + secondAttempt + "\t" + buyerCity + "\t" + sellerCity;
 	}
 }
